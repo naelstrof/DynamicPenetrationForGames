db622510baf78d358d2cf19be44c18394ee76d7e
naelstrof <naelstrof@gmail.com>
2024-03-09 22:07:53 -0700

    responsibility flipping.

--- PenetratorBasic.cs
+++ PenetratorBasic.cs
@@ -13,17 +13,8 @@ public class PenetratorBasicInspector : PenetratorInspector { }
 [ExecuteAlways]
 public class PenetratorBasic : Penetrator {
     [SerializeField] private Transform[] transforms;
-    [SerializeField] private PenetratorRenderers penetratorRenderers;
     
     private List<Vector3> points = new();
-
-    private void OnEnable() {
-        penetratorRenderers.Initialize();
-    }
-
-    private void OnDisable() {
-        penetratorRenderers.OnDestroy();
-    }
     
     protected override IList<Vector3> GetPoints() {
         points.Clear();
@@ -31,20 +22,5 @@ public class PenetratorBasic : Penetrator {
             points.Add(t.position);
         }
         return points;
-    }
-
-    protected void Update() {
-        if (transforms == null || transforms.Length == 0) {
-            return;
-        }
-        penetratorData.GetSpline(GetPoints(), out var path, out float distanceAlongSpline);
-        penetratorRenderers.Update(path, distanceAlongSpline, penetratorData.GetRootTransform(), penetratorData.GetRootForward(), penetratorData.GetRootRight(), penetratorData.GetRootUp());
-    }
-
-    protected override void OnDrawGizmos() {
-        if (transforms == null || transforms.Length == 0) {
-            return;
-        }
-        base.OnDrawGizmos();
     }
 }
